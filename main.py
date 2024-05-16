cea61319453a71842fb8f2cab36d5fda9f60d849
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-05-16 14:12:21 +0100

    Restore '5/16/2024, 1:59:06 PM w1 content'

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+# GH1 - Jump pads
+# BH1 - Limit platforms
+# BH2 - Flying enemies to attack minions
+# BH3 - Create more levels
+
 @namespace
 class SpriteKind:
     collider = SpriteKind.create()
+    # GH1
+    jump = SpriteKind.create()
+    # /GH1
 
 # variables
 levels = [
@@ -8,16 +16,37 @@ levels = [
     assets.tilemap("level 2"),
     assets.tilemap("level 3"),
     assets.tilemap("level 4"),
+    #BH3
+    # simply designing a new tilemap and entering it into the levels list
+    # assets.tilemap("level 5")
+    assets.tilemap("level 5"),
+    # /BH3
+    #GH1
+    # maybe invite coders in the last step to produce their own tilemap that requires a jump pad
 ]
-
 level = -1
 wave_size = 14
 speed = 20
 escaped_minions = 0
+# BH1
+platforms_available = 0
+# /BH1
 spawning_phase = False
 
 # sprites
 cursor = sprites.create(image.create(2, 2))
+# BH1
+platforms_counter = textsprite.create(str(platforms_available))
+platforms_counter.set_flag(SpriteFlag.RELATIVE_TO_CAMERA, True)
+# /BH1
+
+# BH1
+def update_platforms_counter():
+    platforms_counter.set_text(str(platforms_available + " platforms"))
+    platforms_counter.left = 0
+    platforms_counter.bottom = 120
+update_platforms_counter()
+# /BH1
 
 def next_level():
     global level
@@ -27,13 +56,22 @@ def next_level():
 next_level()
 
 def reset_level():
-    global escaped_minions
+    global escaped_minions, platforms_available # bh1 edit line
     if not spawning_phase:
+        # BH1
+        platforms_available = level + 3
+        # /BH1
+        escaped_minions = 0
+        sprites.destroy_all_sprites_of_kind(SpriteKind.player)
+        # GH1
+        sprites.destroy_all_sprites_of_kind(SpriteKind.jump)
+        # /GH1
+        # BH2
+        sprites.destroy_all_sprites_of_kind(SpriteKind.enemy)
+        # /BH2
         for location in tiles.get_tiles_by_type(assets.tile("platform")):
             tiles.set_tile_at(location, assets.tile("empty"))
             tiles.set_wall_at(location, False)
-        escaped_minions = 0
-        sprites.destroy_all_sprites_of_kind(SpriteKind.player)
         tiles.set_current_tilemap(levels[level])
         music.beam_up.play()
         timer.background(spawn_minions)
@@ -46,8 +84,8 @@ def spawn_minions():
     for i in range(wave_size):
         pause(750)
         minion = sprites.create(assets.image("minion"), SpriteKind.player)
+        sprites.set_data_number(minion, "x_vel", speed)
         tiles.place_on_random_tile(minion, assets.tile("spawn"))
-        minion.vx = speed
         minion.ay = 120
         characterAnimations.loop_frames(minion, assets.animation("walk right"), 100, Predicate.MOVING_RIGHT)
         characterAnimations.loop_frames(minion, assets.animation("walk left"), 100, Predicate.MOVING_LEFT)
@@ -56,7 +94,7 @@ def spawn_minions():
 
 def mouse_behaviour():
     cursor.x = browserEvents.get_mouse_scene_x()
-    cursor.y = browserEvents.get_mouse_scene_y() 
+    cursor.y = browserEvents.get_mouse_scene_y()
 
 def camera_movement():
     camera_x = scene.camera_property(CameraProperty.X)
@@ -71,18 +109,86 @@ def camera_movement():
         scene.center_camera_at(camera_x, camera_y + 1)
 
 def minion_movement():
+    number = 0
     for minion in sprites.all_of_kind(SpriteKind.player):
         if minion.is_hitting_tile(CollisionDirection.LEFT):
-            minion.vx = speed
+            sprites.set_data_number(minion, "x_vel", speed)
         elif minion.is_hitting_tile(CollisionDirection.RIGHT):
-            minion.vx = speed * -1
+            sprites.set_data_number(minion, "x_vel", speed * -1)
+        minion.vx = sprites.read_data_number(minion, "x_vel")
 game.on_update(minion_movement)
 
 def place_platform(x, y):
+    # BH1
+    global platforms_available
+    if platforms_available < 1:
+        return
+    # /BH1
     if tiles.tile_at_location_equals(cursor.tilemap_location(), assets.tile("empty")):
+    # BH1
+        platforms_available -= 1
+        update_platforms_counter()
+    # /BH1
         tiles.set_tile_at(cursor.tilemap_location(), assets.tile("platform"))
         tiles.set_wall_at(cursor.tilemap_location(), True)
 browserEvents.mouse_left.on_event(browserEvents.MouseButtonEvent.PRESSED, place_platform)
+
+# BH2
+def spawn_bat():
+    if len(sprites.all_of_kind(SpriteKind.player)) < 1:
+        return
+    bat = tilesAdvanced.create_pathfinder_sprite(assets.image("bat"), SpriteKind.enemy)
+    animation.run_image_animation(bat, assets.animation("bat flight"), 50, True)
+    map_width = tilesAdvanced.get_tilemap_width()
+    tiles.place_on_tile(bat, tiles.get_tile_location(randint(0, map_width), 0))
+    target = sprites.all_of_kind(SpriteKind.player)._pick_random()
+    tilesAdvanced.follow_using_pathfinding(bat, target, 15)
+game.on_update_interval(10000, spawn_bat)
+
+def minion_caught(minion, bat):
+    minion.destroy()
+    bat.destroy()
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, minion_caught)
+
+def hit_bat(collider, bat):
+    info.change_score_by(10)
+    bat.destroy()
+sprites.on_overlap(SpriteKind.collider, SpriteKind.enemy, hit_bat)
+# /BH2
+
+# GH1
+def place_jump_pad():
+    cursor_location = cursor.tilemap_location()
+    if tiles.tile_at_location_equals(cursor_location, assets.tile("empty")):
+        pad = sprites.create(assets.image("jump pad light"), SpriteKind.jump)
+        sprites.set_data_number(pad, "jump_strength", -100)
+        tiles.place_on_tile(pad, cursor_location)
+controller.A.on_event(ControllerButtonEvent.PRESSED, place_jump_pad)
+
+def jump(sprite, pad):
+    jump_strength = sprites.read_data_number(pad, "jump_strength")
+    sprite.vy = jump_strength
+sprites.on_overlap(SpriteKind.player, SpriteKind.jump, jump)
+
+def cycle_jump_pad(collider, pad):
+    if pad.image.equals(assets.image("jump pad light")):
+        pad.set_image(assets.image("jump pad strong"))
+        sprites.set_data_number(pad, "jump_strength", -150)
+    else:
+        pad.set_image(assets.image("jump pad light"))
+        sprites.set_data_number(pad, "jump_strength", -75)
+    collider.destroy()
+sprites.on_overlap(SpriteKind.collider, SpriteKind.jump, cycle_jump_pad)
+
+# ---- Also required for BH2
+def click_interact(x, y):
+    collider = sprites.create(image.create(5, 5), SpriteKind.collider)
+    collider.set_position(cursor.x, cursor.y)
+    collider.image.fill(2)
+    collider.set_flag(SpriteFlag.INVISIBLE, True)
+    collider.lifespan = 500
+browserEvents.mouse_wheel.on_event(browserEvents.MouseButtonEvent.PRESSED, click_interact)
+# /GH1
 
 def end_reached(minion, location):
     global escaped_minions
@@ -99,9 +205,9 @@ def check_remaining_minions():
         if escaped_minions > 3:
             if level > len(levels) - 1:
                 game.over(True)
-            else: 
+            else:
                 next_level()
-        else: 
+        else:
             reset_level()
 sprites.on_destroyed(SpriteKind.player, check_remaining_minions)
 
--- main.ts
+++ main.ts
@@ -1,16 +1,45 @@
+//  GH1 - Jump pads
+//  BH1 - Limit platforms
+//  BH2 - Flying enemies to attack minions
+//  BH3 - Create more levels
 namespace SpriteKind {
     export const collider = SpriteKind.create()
+    //  GH1
+    export const jump = SpriteKind.create()
 }
 
+//  /GH1
 //  variables
-let levels = [assets.tilemap`level 1`, assets.tilemap`level 2`, assets.tilemap`level 3`, assets.tilemap`level 4`]
+let levels = [assets.tilemap`level 1`, assets.tilemap`level 2`, assets.tilemap`level 3`, assets.tilemap`level 4`, assets.tilemap`level 5`]
+// BH3
+//  simply designing a new tilemap and entering it into the levels list
+//  assets.tilemap("level 5")
+//  /BH3
+// GH1
+//  maybe invite coders in the last step to produce their own tilemap that requires a jump pad
 let level = -1
 let wave_size = 14
 let speed = 20
 let escaped_minions = 0
+//  BH1
+let platforms_available = 0
+//  /BH1
 let spawning_phase = false
 //  sprites
 let cursor = sprites.create(image.create(2, 2))
+//  BH1
+let platforms_counter = textsprite.create("" + platforms_available)
+platforms_counter.setFlag(SpriteFlag.RelativeToCamera, true)
+//  /BH1
+//  BH1
+function update_platforms_counter() {
+    platforms_counter.setText("" + (platforms_available + " platforms"))
+    platforms_counter.left = 0
+    platforms_counter.bottom = 120
+}
+
+update_platforms_counter()
+//  /BH1
 function next_level() {
     
     info.changeScoreBy(escaped_minions * 10)
@@ -21,13 +50,23 @@ function next_level() {
 next_level()
 function reset_level() {
     
+    //  bh1 edit line
     if (!spawning_phase) {
+        //  BH1
+        platforms_available = level + 3
+        //  /BH1
+        escaped_minions = 0
+        sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+        //  GH1
+        sprites.destroyAllSpritesOfKind(SpriteKind.jump)
+        //  /GH1
+        //  BH2
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        //  /BH2
         for (let location of tiles.getTilesByType(assets.tile`platform`)) {
             tiles.setTileAt(location, assets.tile`empty`)
             tiles.setWallAt(location, false)
         }
-        escaped_minions = 0
-        sprites.destroyAllSpritesOfKind(SpriteKind.Player)
         tiles.setCurrentTilemap(levels[level])
         music.beamUp.play()
         timer.background(function spawn_minions() {
@@ -38,8 +77,8 @@ function reset_level() {
             for (let i = 0; i < wave_size; i++) {
                 pause(750)
                 minion = sprites.create(assets.image`minion`, SpriteKind.Player)
+                sprites.setDataNumber(minion, "x_vel", speed)
                 tiles.placeOnRandomTile(minion, assets.tile`spawn`)
-                minion.vx = speed
                 minion.ay = 120
                 characterAnimations.loopFrames(minion, assets.animation`walk right`, 100, Predicate.MovingRight)
                 characterAnimations.loopFrames(minion, assets.animation`walk left`, 100, Predicate.MovingLeft)
@@ -75,22 +114,92 @@ function camera_movement() {
 }
 
 game.onUpdate(function minion_movement() {
+    let number = 0
     for (let minion of sprites.allOfKind(SpriteKind.Player)) {
         if (minion.isHittingTile(CollisionDirection.Left)) {
-            minion.vx = speed
+            sprites.setDataNumber(minion, "x_vel", speed)
         } else if (minion.isHittingTile(CollisionDirection.Right)) {
-            minion.vx = speed * -1
+            sprites.setDataNumber(minion, "x_vel", speed * -1)
         }
         
+        minion.vx = sprites.readDataNumber(minion, "x_vel")
     }
 })
 browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function place_platform(x: any, y: any) {
+    //  BH1
+    
+    if (platforms_available < 1) {
+        return
+    }
+    
+    //  /BH1
     if (tiles.tileAtLocationEquals(cursor.tilemapLocation(), assets.tile`empty`)) {
+        //  BH1
+        platforms_available -= 1
+        update_platforms_counter()
+        //  /BH1
         tiles.setTileAt(cursor.tilemapLocation(), assets.tile`platform`)
         tiles.setWallAt(cursor.tilemapLocation(), true)
     }
     
 })
+//  BH2
+game.onUpdateInterval(10000, function spawn_bat() {
+    if (sprites.allOfKind(SpriteKind.Player).length < 1) {
+        return
+    }
+    
+    let bat = tilesAdvanced.createPathfinderSprite(assets.image`bat`, SpriteKind.Enemy)
+    animation.runImageAnimation(bat, assets.animation`bat flight`, 50, true)
+    let map_width = tilesAdvanced.getTilemapWidth()
+    tiles.placeOnTile(bat, tiles.getTileLocation(randint(0, map_width), 0))
+    let target = sprites.allOfKind(SpriteKind.Player)._pickRandom()
+    tilesAdvanced.followUsingPathfinding(bat, target, 15)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function minion_caught(minion: Sprite, bat: Sprite) {
+    minion.destroy()
+    bat.destroy()
+})
+sprites.onOverlap(SpriteKind.collider, SpriteKind.Enemy, function hit_bat(collider: Sprite, bat: Sprite) {
+    info.changeScoreBy(10)
+    bat.destroy()
+})
+//  /BH2
+//  GH1
+controller.A.onEvent(ControllerButtonEvent.Pressed, function place_jump_pad() {
+    let pad: Sprite;
+    let cursor_location = cursor.tilemapLocation()
+    if (tiles.tileAtLocationEquals(cursor_location, assets.tile`empty`)) {
+        pad = sprites.create(assets.image`jump pad light`, SpriteKind.jump)
+        sprites.setDataNumber(pad, "jump_strength", -100)
+        tiles.placeOnTile(pad, cursor_location)
+    }
+    
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.jump, function jump(sprite: Sprite, pad: Sprite) {
+    let jump_strength = sprites.readDataNumber(pad, "jump_strength")
+    sprite.vy = jump_strength
+})
+sprites.onOverlap(SpriteKind.collider, SpriteKind.jump, function cycle_jump_pad(collider: Sprite, pad: Sprite) {
+    if (pad.image.equals(assets.image`jump pad light`)) {
+        pad.setImage(assets.image`jump pad strong`)
+        sprites.setDataNumber(pad, "jump_strength", -150)
+    } else {
+        pad.setImage(assets.image`jump pad light`)
+        sprites.setDataNumber(pad, "jump_strength", -75)
+    }
+    
+    collider.destroy()
+})
+//  ---- Also required for BH2
+browserEvents.MouseWheel.onEvent(browserEvents.MouseButtonEvent.Pressed, function click_interact(x: any, y: any) {
+    let collider = sprites.create(image.create(5, 5), SpriteKind.collider)
+    collider.setPosition(cursor.x, cursor.y)
+    collider.image.fill(2)
+    collider.setFlag(SpriteFlag.Invisible, true)
+    collider.lifespan = 500
+})
+//  /GH1
 scene.onOverlapTile(SpriteKind.Player, assets.tile`end`, function end_reached(minion: Sprite, location: tiles.Location) {
     
     escaped_minions += 1
